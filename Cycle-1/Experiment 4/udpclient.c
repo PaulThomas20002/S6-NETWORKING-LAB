43905fdb2baa0b7fda0b1e05bb2f05c10f72c1a3
Ajai <95859208+ajaikumarvs@users.noreply.github.com>
2023-04-24 11:12:32 +0530

    Create udpclient.c

---
+++ Cycle-1/Experiment 4/udpclient.c
@@ -0,0 +1,43 @@
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#define MAX 80
+#define PORT 8088
+#define SA struct sockaddr
+int main()
+{
+char buff[MAX];
+int sockfd,len,n;
+struct sockaddr_in servaddr;
+sockfd=socket(AF_INET,SOCK_DGRAM,0);
+if(sockfd==-1)
+{
+printf("socket creation failed...\n");
+exit(0);
+}
+else
+printf("Socket successfully created..\n");
+bzero(&servaddr,sizeof(len));
+servaddr.sin_family=AF_INET;
+servaddr.sin_addr.s_addr=inet_addr("10.0.12.4");
+servaddr.sin_port=htons(PORT);
+len=sizeof(servaddr);
+for(;;)
+{
+printf("\nEnter string : ");
+n=0;
+while((buff[n++]=getchar())!='\n');
+sendto(sockfd,buff,sizeof(buff),0,(SA *)&servaddr,len);
+bzero(buff,sizeof(buff));
+recvfrom(sockfd,buff,sizeof(buff),0,(SA *)&servaddr,&len);
+printf("From Server : %s\n",buff);
+if(strncmp("exit",buff,4)==0)
+{
+printf("Client Exit...\n");
+break;
+}
+}
+close(sockfd);
+}
